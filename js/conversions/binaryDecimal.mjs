b18eec60779d4164814ebdd517afbeba9348ab8f
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-02-18 11:12:06 -0800

    Convert binary to decimal algorithm

---
+++ js/conversions/binaryDecimal.mjs
@@ -0,0 +1,34 @@
+import { Validation } from '../validation/systemValidation.mjs'
+
+// Binary to Decimal logic function
+export default function toDecimal(number, callback) {
+
+  Validation.validateBinary(number)
+  
+  let inputLength = number.length // Get length of the user input
+  let binaryArray = number.split('').map(e => Number(e)) // Save user input value into an array
+  
+  let potentialNumbers = [] // Define an array for potential numbers
+
+  // Loop to create the potential numbers an push them into the array created
+  for (let index = 0; index < inputLength; index++) {
+    let element = Math.pow(2, index)
+    potentialNumbers.push(element)
+  }
+
+  potentialNumbers = potentialNumbers.reverse() // Invert the array
+
+  let sum = 0; // Define sum variable
+  
+  // Loop through the binary number to evaluate
+  binaryArray.forEach((element, index) => {
+    // In case an element is one, sum the corresponding potential number value
+    if (element == 1) {
+      sum += potentialNumbers[index];
+    }
+  });
+
+  callback(null, sum)  
+  
+}
+
